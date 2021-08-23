a3c87ae1a5f4c42e4be90ec799b82a0dac70a672
enriquegarcia96 <enriquegarcia259@yahoo.es>
2021-08-23 16:46:10 -0600

    feat: 🎸 creando las rutas de la pagina

--- app/mail.py
+++ app/mail.py
@@ -2,11 +2,23 @@ from flask import (
     Blueprint,
     render_template
 )
-from flask.templating import render_template
+from app.db import get_db
+
+#from flask.templating import render_template
 
 bp = Blueprint('mail', __name__, url_prefix='/')
 
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('mails/index.html')
+    db, c = get_db()
+    c.execute("SELECT * FROM email")
+    mails = c.fetchall()
+
+    return render_template('mails/index.html', mails=mails)
+
+
+
+@bp.route('/create', methods=['GET','POST'])
+def create():
+    return render_template('mails/create.html')
